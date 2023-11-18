c61441e82ca8c02642972d64091f019212b7c17a
Kaike Miguel <132000016+KaikeMiguel@users.noreply.github.com>
2023-11-17 23:43:44 -0300

    Update Determinante de Matriz.py

--- Determinante de Matriz.py
+++ Determinante de Matriz.py
@@ -1,121 +1,143 @@
 print("--------DETERMINANTE DE MATRIZ--------\n")
 
-mtx_linha = int(input("Digite o tamanho da matriz quadrada: "))
-mtx_coluna = mtx_linha
-mtx_size = mtx_linha
+mtx_size = int(input("Digite o tamanho da MATRIZ: "))
 
-matriz = []
+matrix = []
+mtx_column = mtx_size
+mtx_line = mtx_size
 
-# ======================================================
-# Criação da Matriz Quadrada
 
-for linha in range(mtx_linha):
-    new_linha = []
+# -- CRIAÇÃO DA MATRIZ QUADRADA --
+
+for line in range(mtx_line):
+    new_line = []
     
-    for coluna in range(mtx_coluna):
-        new_linha.append(int(input(f"{linha+1}m X {coluna+1}n: ")))
+    for column in range(mtx_column):
+        new_line.append(int(input(f"{line+1}m X {column+1}n: ")))
         
+    matrix.append(new_line)
     
-    matriz.append(new_linha)
+    
+# ==================================================================
+# -- CÁLCULO DO Sn --
+
+def SnFunc(mtx_size):
+    i = 1
+    Sn = 1
+    
+    while i <= mtx_size:
+        Sn *= i
+        i += 1
+    
+    return Sn
+
+Sn = SnFunc(mtx_size)
 
 
+# ==================================================================
+# -- SIGMA (σ) --
+
+def sigmaFunc(mtx_size):
+    sigma = []
+    sgn = ''
+    i = 1
+    
+    while i <= mtx_size:
+        sgn += str(i)
+        i += 1
+    
+    # -- PERMUTAÇÃO --
+    
+    def permutation(lst):
+        if len(lst) < 2:
+            return [lst]
+        
+        l = []
+        
+        for i in range(len(lst)):
+           m = lst[i]
+           remLst = lst[:i] + lst[i+1:]
+           
+           for p in permutation(remLst):
+               l.append([m] + p)
+               
+        return l
+    
+    
+    for i in permutation([i+1 for i in range(mtx_size)]):
+        sigma.append("".join(map(str,i)))
+    
+    return sigma
+    
+sigma = sigmaFunc(mtx_size)
+
+
+# -- RESULTADO DA PERMUTAÇÃO --
+    
+def permResult(sigma):
+    base = sigma[0]   
+    sigma_changes = []
+        
+    for i in range(len(sigma)):
+        count = 0
+        diff = sigma[i]
+            
+        for j in range(len(base)):
+            count += diff[j] != base[j]
+            
+        resp = count -1 if count > 0 else 0
+        sigma_changes.append(resp)
+        
+    return sigma_changes
+
+sign_sigma = permResult(sigmaFunc(mtx_size))
+
+    
+# ==================================================================     
+# -- PRODUTÓRIO --
+
+
+def product(sigma):
+    prod = []
+    
+    for j in range(len(sigma)):
+        base = sigma[j]
+    
+        for i in range(len(base)):
+            u = base[i]
+            c = 1
+            
+            while u != str(c):
+                c += 1
+              
+            a = matrix[i][c-1]
+            e = ((-1)**sign_sigma[i])*a
+            
+            prod.append(e)
+    
+    return prod
+
+prod = product(sigma)
+
+        
+# ==================================================================     
+# -- SOMATÓRIA --    
+        
+        
+# ==================================================================     
+# -- CONCLUSÃO -- 
+        
 print("\n---------ESTRUTURA DA MATRIZ----------\n")
-for linha in range(mtx_linha):
-    for coluna in range(mtx_coluna):
-        print("[",matriz[linha][coluna], "]",end="")
+for line in range(mtx_line):
+    for column in range(mtx_column):
+        print("[",matrix[line][column], "]",end="")
 
     print()
-
-# ======================================================
-# Calculo do Sn
-
-n = mtx_size
-i = 1
-Sn = 1
-
-while i <= n:
-    Sn *= i
-    i += 1
-
-    
-# ======================================================
-# Funções: sigma (σ)
-
-sigma = []
-j = 1
-sgn = ""    
-
-while j <= mtx_size:
-    sgn += str(j)
-    j+=1
-
-
-def permutation(lst):
-    if len(lst) < 2:
-        return [lst]
-    
-    l = []
-    
-    for i in range(len(lst)):
-       m = lst[i]
-       remLst = lst[:i] + lst[i+1:]
-       
-       for p in permutation(remLst):
-           l.append([m] + p)
-           
-    return l
-
-
-for i in permutation([i+1 for i in range(mtx_size)]):
-    sigma.append("".join(map(str,i)))
-    
-
-def lev(a,b):
-    if len(a) == 0:
-        return len(b)
-    
-    elif len(b) == 0:
-        return len(a)-1
-    
-    elif a[0] == b[0]:
-        return lev(a[1:], b[1:])
-    
-    else:
-        return 1 + min( lev(a[1:], b), lev(a, b[1:]), lev(a[1:], b[1:]) )
-
-    
-# ======================================================
-# Produtório (∏)
-
-def produtorio(e,n):
-    p = 1
-    
-    for i in range(n):
-        p *= e
-
-    return p
-       
-            
-# ======================================================
-# Somatória (∑)
-
-def somatoria(e,n):
-    s = 0
-    
-    for i in range(n):
-        s += e
-
-    return s
-
-    
-# ======================================================
-# Conclusão
 
 
 print("\n-----------RESULTADO FINAL------------\n")
 print("Sn =",Sn)
 print("σ =",sigma)
-print("∏ =",)
+print("∏ =",prod)
 print("∑ =",)
-print("det(A) =",)
-    
+print("det(A) =",)        
